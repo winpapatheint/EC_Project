2e427327ea9e781d91667e6601cabe7d5f739268
THINTHINMONAUNG <143170408+THINTHINMONAUNG@users.noreply.github.com>
2024-03-13 12:01:41 +0900

    update product_all status button change

--- resources/views/seller/product/product_all.blade.php
+++ resources/views/seller/product/product_all.blade.php
@@ -48,8 +48,11 @@
 
                                             <td class="td-price">$95.97</td>
 
-                                            <td class="status-danger">
-                                                <span>Pending</span>
+                                            <td class="col-sm-9">
+                                                <label class="switch">
+                                                    <input type="checkbox" checked=""><span
+                                                        class="switch-state"></span>
+                                                </label>
                                             </td>
 
                                             <td>

8ebbe9e7ed91e81d07988185ba3bc7e69a0c2aa4
THINTHINMONAUNG <143170408+THINTHINMONAUNG@users.noreply.github.com>
2024-03-13 14:28:54 +0900

    Update product_all.blade.php

--- resources/views/seller/product/product_all.blade.php
+++ resources/views/seller/product/product_all.blade.php
@@ -94,6 +94,12 @@
                                             <td>10</td>
 
                                             <td class="td-price">$95.97</td>
+                                            <td class="col-sm-9">
+                                                <label class="switch">
+                                                    <input type="checkbox" checked=""><span
+                                                        class="switch-state"></span>
+                                                </label>
+                                            </td>
 
                                             <td>
                                                 <ul>

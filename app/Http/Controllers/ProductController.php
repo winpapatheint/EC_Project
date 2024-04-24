cd38832316a9332664e8ef4775697944ca1e55df
PC_11 <winpapatheint77@gmail.com>
2024-04-24 17:17:16 +0900

    added necessary file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,6 +17,7 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Haruncpi\LaravelIdGenerator\IdGenerator;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {

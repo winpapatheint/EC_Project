04031859e17f002dc46e591059d87e879c6ad5b6
Zarle91 <zarlezhon48@gmail.com>
2024-05-10 14:39:59 +0900

    discount percent 0

--- resources/views/front-end/sub-category-left-sidebar.blade.php
+++ resources/views/front-end/sub-category-left-sidebar.blade.php
@@ -541,7 +541,7 @@
                         <div class="col-lg-6">
                             <div class="right-sidebar-modal">
                                 <h4 class="title-name">{{ $product->product_name }}</h4>
-                                @if ($product->discount_percent != null || $discount->discount_percent != 0)
+                                @if ($product->discount_percent != null || $product->discount_percent != 0)
                                     <h4 class="price"><span class="theme-color">¥{{ number_format($product->selling_price, 0, '', ',') }}</span>
                                     <del>¥{{ number_format($product->original_price, 0, '', ',') }}</del>
                                 @else

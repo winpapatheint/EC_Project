db994c23dcb29e796f29f4b376ebed8eda7cac66
THINTHINMONAUNG <163237398+HanakoTtma@users.noreply.github.com>
2024-05-15 15:49:58 +0900

    mail update

--- app/Models/Help.php
+++ app/Models/Help.php
@@ -8,15 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Help extends Model
 {
     use HasFactory;
-<<<<<<< HEAD
-    protected $table = 'helps';
-    protected $fillable = [
-        'user_id',
-        'title',
-        'reason',
-        'img',
-
-=======
 
     protected $fillable = [
         'help_id',
@@ -26,6 +17,5 @@ class Help extends Model
         'subject',
         'body',
         'img',
->>>>>>> f4c0d3dbb4cd28ea174c0055c1e4f6c282e3b63e
     ];
 }

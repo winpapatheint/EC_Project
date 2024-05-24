17347898b3e6e32c94324c93ee51e67a6d677b63
Zarle91 <zarlezhon48@gmail.com>
2024-05-24 17:28:42 +0900

    theme css

--- public/frontend/assets/js/theme-setting.js
+++ public/frontend/assets/js/theme-setting.js
@@ -11,37 +11,61 @@ document.getElementById("colorPick").onchange = function () {
 /*========================
  Dark setting js
  ==========================*/
+// $("#darkButton").on("click", function () {
+//     var href = $("#color-link").attr("href");
+//     $("body").removeClass("light");
+//     $("body").addClass("dark");
+//     document
+//         .getElementById("color-link")
+//         .setAttribute("href", "frontend/assets/css/dark.css");
+// });
+
+// $("#lightButton").on("click", function () {
+//     var href = $("#color-link").attr("href");
+//     $("body").removeClass("dark");
+//     $("body").addClass("light");
+//     document
+//         .getElementById("color-link")
+//         .setAttribute("href", "frontend/assets/css/style.css");
+// });
+
+// Dark mode button
 $("#darkButton").on("click", function () {
-    var href = $("#color-link").attr("href");
-    $("body").removeClass("light");
-    $("body").addClass("dark");
-    document
-        .getElementById("color-link")
-        .setAttribute("href", "frontend/assets/css/dark.css");
+    $("body").removeClass("light").addClass("dark");
+    document.getElementById("color-link").setAttribute("href", "/frontend/assets/css/dark.css");
 });
 
+// Light mode button
 $("#lightButton").on("click", function () {
-    var href = $("#color-link").attr("href");
-    $("body").removeClass("dark");
-    $("body").addClass("light");
-    document
-        .getElementById("color-link")
-        .setAttribute("href", "frontend/assets/css/style.css");
+    console.log("light");
+    $("body").removeClass("dark").addClass("light");
+    document.getElementById("color-link").setAttribute("href", "/frontend/assets/css/style.css");
 });
 
 /*========================
    RTL setting js
    ==========================*/
+// $(".rtl").on("click", function () {
+//     if ($("body").hasClass("ltr")) {
+//         $("html").attr("dir", "rtl");
+//         $("body").removeClass("ltr");
+//         $("body").addClass("rtl");
+//         $("#rtl-link").attr("href", "frontend/assets/css/vendors/bootstrap.rtl.css");
+//     } else {
+//         $("html").attr("dir", "");
+//         $("body").removeClass("rtl");
+//         $("body").addClass("ltr");
+//         $("#rtl-link").attr("href", "frontend/assets/css/vendors/bootstrap.css");
+//     }
+// });
 $(".rtl").on("click", function () {
     if ($("body").hasClass("ltr")) {
         $("html").attr("dir", "rtl");
-        $("body").removeClass("ltr");
-        $("body").addClass("rtl");
-        $("#rtl-link").attr("href", "frontend/assets/css/vendors/bootstrap.rtl.css");
+        $("body").removeClass("ltr").addClass("rtl");
+        $("#rtl-link").attr("href", "/frontend/assets/css/vendors/bootstrap.rtl.css");
     } else {
         $("html").attr("dir", "");
-        $("body").removeClass("rtl");
-        $("body").addClass("ltr");
-        $("#rtl-link").attr("href", "frontend/assets/css/vendors/bootstrap.css");
+        $("body").removeClass("rtl").addClass("ltr");
+        $("#rtl-link").attr("href", "/frontend/assets/css/vendors/bootstrap.css");
     }
 });

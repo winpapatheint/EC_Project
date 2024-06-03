582492b1e9bbe5407e5aad6b85b5d4057791cfac
Zarle91 <zarlezhon48@gmail.com>
2024-06-03 17:05:46 +0900

    blog detail

--- resources/views/front-end/blog-detail.blade.php
+++ resources/views/front-end/blog-detail.blade.php
@@ -29,28 +29,7 @@
     <section class="blog-section section-b-space">
         <div class="container-fluid-lg">
             <div class="row g-4">
-                <div class="col-xxl-9 col-xl-8 col-lg-7 order-lg-2">
-                    <div class="row g-4">
-                        <div class="col-12">
-                            <div class="blog-box blog-list wow fadeInUp">
-                                <div class="blog-image">
-                                    <img src="{{ asset('images/'.$blog->image ) }}" class="blur-up lazyload" alt="">
-                                </div>
-
-                                <div class="blog-contain blog-contain-2">
-                                    <div class="blog-label">
-                                        <span class="time"><i data-feather="clock"></i> <span> {{ date('Y/m/d', strtotime($blog->created_at)) }} </span></span>
-                                        <span class="super"><i data-feather="user"></i> <span>{{ $blog->authorby }}</span></span>
-                                    </div>
-                                    <p>{!! $blog->content !!}</p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xxl-3 col-xl-4 col-lg-5 order-lg-1">
+                <div class="col-xxl-3 col-xl-4 col-lg-5">
                     <div class="left-sidebar-box wow fadeInUp">
                     <form id="searchForm" action="/news" method="GET">
                         <div class="accordion-item">
@@ -97,7 +76,58 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xxl-9 col-xl-8 col-lg-7 ratio_50">
+                    <div class="blog-detail-image rounded-3 mb-4">
+                        <img src="{{ asset('images/'.$blog->image ) }}" class="bg-img blur-up lazyload" alt="">
+                        <div class="blog-image-contain">
+                            <h2>{{ $blog->title }}</h2>
+                            <ul class="contain-comment-list">
+                                <li>
+                                    <div class="user-list">
+                                        <i data-feather="user"></i>
+                                        <span>{{ $blog->authorby }}</span>
+                                    </div>
+                                </li>
 
+                                <li>
+                                    <div class="user-list">
+                                        <i data-feather="calendar"></i>
+                                        <span>{{ date('Y/m/d', strtotime($blog->created_at)) }}</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    @php
+                        $content = $blog->content;
+                        $content = str_replace(['<p>', '</p>'], '', $content);
+                        $midIndex = mb_strlen($content) / 2;
+                        $splitIndex = mb_strpos($content, '.', $midIndex);
+                        if ($splitIndex === false) {
+                            $splitIndex = $midIndex;
+                        } else {
+                            $splitIndex += 1;
+                        }
+
+                        $firstHalf = mb_substr($content, 0, $splitIndex);
+                        $secondHalf = mb_substr($content, $splitIndex);
+                    @endphp
+        
+                    <div class="blog-detail-contain">
+                        <p>
+                            <span class="first">{!! mb_substr($firstHalf, 0, 1) !!}</span>{!! mb_substr($firstHalf, 1) !!}
+                        </p>
+
+                        
+                        <div class="blog-details-quote">
+                            <h3>{{ $blog->title}}</h3>
+                            <h5>- {{ $blog->authorby }}</h5>
+                        </div>
+
+                        <p>&emsp;&emsp;{{ $secondHalf }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>

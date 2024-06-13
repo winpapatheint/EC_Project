7f1b8af0c452974257eada697dca7b63ea6b65cd
PC_11 <winpapatheint77@gmail.com>
2024-06-13 18:27:00 +0900

    modify help

--- resources/views/emails/admin_contact.blade.php
+++ resources/views/emails/admin_contact.blade.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Seller Contact</title>
+    <title>Admin Contact</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
+            font-family: 'Times New Roman', Times, serif; /* Change font family to Times New Roman */
             margin: 0;
             padding: 0;
             color: #333;
@@ -31,39 +31,20 @@
             margin: 20px 0;
         }
         .content p {
-            font-size: 16px;
+            font-size: 15px;
             line-height: 1.6;
-            margin-left: 20px;
         }
+
         .content h2 {
             font-size: 24px;
             margin-top: 0;
-
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-
-        th {
-            background-color: #f2f2f2;
-        }
-        .subtotal {
-            font-weight: bold;
         }
         .footer {
             text-align: right;
             margin-top: 40px;
         }
         .footer h3 {
-            font-size: 19px;
+            font-size: 15px;
             color: #333;
         }
         .footer p {
@@ -71,16 +52,30 @@
             color: #777;
             margin: 0;
         }
-        .content h3 {
-            font-size: 19px;
+        .content h3.greet {
+            font-size: 15px;
             margin-top: 20px;
-            margin-left: 50px;
+            margin-left: 0; /* Remove left margin */
+            text-align: left; /* Center-align the heading */
+            font-weight: normal; /* Make the Dear Admin not bold */
+        }
+        .content p.date {
+            text-align: right; /* Align date to the right */
         }
 
         .content p.detail {
             font-size: 16px;
             line-height: 1.6;
-            margin-left: 70px;
+            margin-left: 40px;
+            font-weight: 100px;
+        }
+        .bold-text {
+            font-weight: bold;
+            font-size: 15px;
+        }
+        .normal-text {
+            font-weight: normal;
+            font-size: 15px;
         }
     </style>
 </head>
@@ -91,18 +86,19 @@
             <img src="{{ $message->embed(public_path('images/logos/MailHeader_AFM.png')) }}" alt="Asian Food Museum Logo">
         </div>
         <div class="content">
-            <p>{{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
-            <h3>Dear Admin,</h3>
+            <p class="date">{{ \Carbon\Carbon::now()->format('F j, Y') }}</p>
+            <h3 style="text-align: center"><span class="bold-text">Received</span> <span class="normal-text">a Message!</span></h3>
+            <h3 class="greet">Dear Admin,</h3>
             <p class="detail"><strong>Subject:</strong> {{ $data['title'] }}</p>
             <p class="detail"><strong>Message Details:</strong> {{ $data['content'] }}</p>
             <!-- Embedded Image -->
             <p class="detail"><img src="{{ $message->embed($imagePath) }}" alt="Embedded Image"></p>
-
         </div>
         <div class="footer">
-            <h3 style="text-align: right">Best regards,</h3>
+            <p style="text-align: right;font-weight:100px"><strong>Best regards,</strong></p>
             <p>{{ $data['selleremail'] }}</p>
         </div>
     </div>
 </body>
 </html>
+

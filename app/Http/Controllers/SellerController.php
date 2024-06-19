4b2ec2c9c50faebf6913386e2ffb0c89a89076af
ksithu555 <ksithu555@gmail.com>
2024-06-19 14:44:34 +0900

    new sub seller mail and noti

--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -415,6 +415,19 @@ class SellerController extends Controller
             'password' => Hash::make($validatedData['passwords']),
             'phone' => $request->input('phone'),
         ]);
+        
+        $admins = User::where('role', 'admin')->get();
+        foreach ($admins as $admin) {
+            \Mail::to($admin->email)->send(new \App\Mail\AdminNewMemberRegistration($user, $admin));
+        }
+
+        $createdBy = Seller::where('user_id', $user->created_by)->first();
+        Notification::create([
+            'related_id' => $user->id,
+            'message' => 'A new sub seller added by ' . $createdBy->shop_name . ':',
+            'time' => Carbon::now(),
+            'seen' => 0,
+        ]);
 
         $msg = ('Data added successfully');
         return redirect('/subsellerlist')->with('success', $msg);

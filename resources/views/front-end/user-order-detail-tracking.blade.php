0a02968972421c7c8ca573e6283b3aad51e8d769
ksithu555 <ksithu555@gmail.com>
2024-06-25 11:55:55 +0900

    my menu order link active

--- resources/views/front-end/user-order-detail-tracking.blade.php
+++ resources/views/front-end/user-order-detail-tracking.blade.php
@@ -75,13 +75,13 @@
                                     data-bs-target="#pills-dashboard" type="button" style="font-size: 14px; text-align: center;"><i data-feather="home"></i>
                                     DashBoard</a>
                             </li>
-                            <li class="nav-item active" role="presentation">
-                                <a class="nav-link" id="pills-order-tab" 
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active" id="pills-order-tab" 
                                     style="font-size: 14px; text-align: center;" href="{{route ('user_order')}}"><i
                                         data-feather="shopping-bag"></i>Orders</a>
                             </li>
                             <li class="nav-item" role="presentation">
-                                <a class="nav-link active" id="delivery-detail" 
+                                <a class="nav-link" id="delivery-detail" 
                                     type="button" style="font-size: 14px; text-align: center;" href="{{route ('user_deivery_status')}}"><i data-feather="box"></i>
                                     Delivery Status</a>
                             </li>

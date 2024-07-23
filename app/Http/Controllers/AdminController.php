6b5c7aa9cd26b59e07be49b3d27c7e4f3cf0c176
ksithu555 <ksithu555@gmail.com>
2024-07-23 16:05:52 +0900

    special corner delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2325,7 +2325,7 @@ class AdminController extends Controller
     {
         if ($request->type == '1') {
             Category::where('id', $request->id)->delete();
-        } else {
+        } elseif ($request->type == '2') {
             $cat = SubCategoryTitle::find($request->id);
             $categoryId = $cat->category_id;
             DB::table('sub_category_titles')->where('id', $request->id)->delete();
@@ -2333,6 +2333,8 @@ class AdminController extends Controller
             if (!$categoryIdExist) {
                 Category::where('id', $categoryId)->delete();
             }
+        } else {
+            SubCategory::where('id', $request->id)->delete();
         }
         return redirect()->back()->with('success', 'Deleted Successfully.');
     }
